0861ed32e3292fd1bd6bb744b1e86bcd98a48546
Jake Watembach <jakewatembach@gmail.com>
2023-01-10 15:13:28 -0700

    Initial Commit

---
+++ tutorial/tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  tutorialApp.swift
+//  tutorial
+//
+//  Created by Jake Watembach on 1/10/23.
+//
+
+import SwiftUI
+
+@main
+struct tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
